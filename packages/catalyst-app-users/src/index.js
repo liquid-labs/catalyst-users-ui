59c236d6210a1c25f7a575f92a25f06b7fc41c33
Zane Rockenbaugh <zane@liquid-labs.com>
2018-11-21 14:31:50 -0600

    removed commented out code generated by create-react-library

--- packages/catalyst-app-users/src/index.js
+++ packages/catalyst-app-users/src/index.js
@@ -11,28 +11,3 @@ export * from './components/hocs/withAuthInfo'
 
 export * from './reducers/sessionReducer'
 export { sessionActions }
-
-
-
-/*import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}*/
