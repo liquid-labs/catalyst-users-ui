d73dadf149406904df944f51d5c906ee61cba4fb
Zane Rockenbaugh <zane@liquid-labs.com>
2018-11-21 14:49:13 -0600

    convert to basic dialog

--- packages/catalyst-app-users/src/components/ui/SignIn.jsx
+++ packages/catalyst-app-users/src/components/ui/SignIn.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { compose } from 'recompose'
+
 import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-
-import LabeledBox from './LabeledBox'
 import { ValidInput } from '@liquid-labs/react-validation'
+
+import withMobileDialog from '@material-ui/core/withMobileDialog'
+
 import { isEmail } from '@liquid-labs/validators'
 
-export const SignIn = ({email, password, onSubmit, onInputChange, error, fieldWatcher, classes}) => {
+const SignInBase = ({email, password, onSubmit, fullScreen, open, onInputChange, error, fieldWatcher, classes}) => {
   const commonFieldProps = {
     onInputChange : onInputChange,
     required      : true,
@@ -19,39 +26,46 @@ export const SignIn = ({email, password, onSubmit, onInputChange, error, fieldWa
   };
 
   return (
-    <LabeledBox className={null/*classes.centeredRoot*/} title="Sign in">
-      <form onSubmit={onSubmit}>
-        <Grid container spacing={16}>
-          {error
-            ? <Grid item xs={12}>
-              <Typography color="error">{error.message}</Typography>
+    <Dialog fullScreen={fullScreen} open={true}>
+      <DialogTitle>Sign in</DialogTitle>
+      <DialogContent>
+        <form onSubmit={onSubmit}>
+          <Grid container spacing={16}>
+            {error
+              ? <Grid item xs={12}>
+                <Typography color="error">{error.message}</Typography>
+              </Grid>
+              : null
+            }
+            <ValidInput
+                label="Email"
+                value={email}
+                validate={isEmail}
+                {...commonFieldProps}
+            />
+            <ValidInput
+                label="Password"
+                value={password}
+                type="password"
+                {...commonFieldProps}
+            />
+            <Grid item xs={12} className={null/*classes.controls*/}>
+              <Button color="primary" type="submit" disabled={!fieldWatcher.isValid()}>Sign In</Button>
             </Grid>
-            : null
-          }
-          <ValidInput
-              label="Email"
-              value={email}
-              validate={isEmail}
-              {...commonFieldProps}
-          />
-          <ValidInput
-              label="Password"
-              value={password}
-              type="password"
-              {...commonFieldProps}
-          />
-          <Grid item xs={12} className={null/*classes.controls*/}>
-            <Button color="primary" type="submit" disabled={!fieldWatcher.isValid()}>Sign In</Button>
+            <Grid item xs={12}>
+              <Link to={'/pw-forget'}>Forgot Password?</Link>
+            </Grid>
+            <Grid item xs={12}>
+              Need an account?
+              <Link to={'/'}>Sign Up</Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Link to={'/pw-forget'}>Forgot Password?</Link>
-          </Grid>
-          <Grid item xs={12}>
-            Need an account?
-            <Link to={'/'}>Sign Up</Link>
-          </Grid>
-        </Grid>
-      </form>
-    </LabeledBox>
-  );
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
 }
+
+export const SignIn = compose(
+  withMobileDialog()
+)(SignInBase)
