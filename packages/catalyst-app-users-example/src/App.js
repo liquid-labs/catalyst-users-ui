ca7c02584db466adc08baf7cbda8cda6b660643b
Zane Rockenbaugh <zane@liquid-labs.com>
2018-11-21 14:17:02 -0600

    reorganized files to prevent npm confusion; updated code to get example running

--- packages/catalyst-app-users-example/src/App.js
+++ packages/catalyst-app-users-example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from '@liquid-labs/catalyst-app-users'
+import { SignIn } from '@liquid-labs/catalyst-app-users'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <SignIn />
       </div>
     )
   }
