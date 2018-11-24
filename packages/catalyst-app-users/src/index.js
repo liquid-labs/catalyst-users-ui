174eda11dd2edd9c0ab146ddbae12103ecc78c66
Zane Rockenbaugh <zane@liquid-labs.com>
2018-11-24 11:08:46 -0600

    updated names
    SignOut to Logout, SignUp to Register, PasswordForget to PasswordRecover

--- packages/catalyst-app-users/src/index.js
+++ packages/catalyst-app-users/src/index.js
@@ -1,9 +1,9 @@
 import * as sessionActions from './actions/sessionActions'
 export * from './components/containers/LoginContainer'
 export * from './components/ui/PasswordChange'
-export * from './components/ui/PasswordForget'
-export * from './components/ui/SignOut'
-export * from './components/ui/SignUp'
+export * from './components/ui/PasswordRecover'
+export * from './components/ui/Logout'
+export * from './components/ui/Register'
 export * from './components/ui/UserAccount'
 
 export * from './components/hocs/checkAccess'
