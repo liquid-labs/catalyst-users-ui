1d39cc641bbc57c73dbadf8d9e658e6bd97f224d
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-02 15:20:56 -0600

    removed debug log

--- src/components/ui/AuthenticatingAppFrame.jsx
+++ src/components/ui/AuthenticatingAppFrame.jsx
@@ -5,8 +5,6 @@ import { AuthenticationDialog } from './AuthenticationDialog'
 
 import { manageAuthentication } from '../hocs/manageAuthentication'
 
-console.log("AppFrame in AuthenticatingAppFrame: " + AppFrame)
-
 const AuthenticatingAppFrameBase = (props) =>
   <AppFrame {...props}>
     <AuthenticationDialog />
