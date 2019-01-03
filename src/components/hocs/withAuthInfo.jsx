abf5eafe64c7b5c67fe9e7d05bcd98d613e1c9e5
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-03 12:31:15 -0600

    simplify and remove the progress bit
    which means we no longer have a peer dep on mui-extensions. I believe the original wait was based on now no-longer necessary logic, and, in any case, it's not necessary here and should be added through extension where needed

--- src/components/hocs/withAuthInfo.jsx
+++ src/components/hocs/withAuthInfo.jsx
@@ -1,31 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-import { CenteredProgress } from '@liquid-labs/mui-extensions'
-
 export const withAuthInfo = (Component) => {
-  class WithAuthInfo extends React.Component {
-    render() {
-      return (
-        <React.Fragment>
-          { !this.props.authResolved && <CenteredProgress /> }
-          { this.props.authResolved && <Component {...this.props} />}
-        </React.Fragment>
-      )
-    }
-  }
-
-  WithAuthInfo.propTypes = {
-    authResolved : PropTypes.bool.isRequried
-  }
-
   const mapStateToProps = (state) => ({
-    authResolved : state.sessionState.authResolved,
-    authUser     : state.sessionState.authUser,
-    authToken    : state.sessionState.authToken,
-    claims       : state.sessionState.claims
-  });
+    authUser  : state.sessionState.authUser,
+    authToken : state.sessionState.authToken,
+    claims    : state.sessionState.claims
+  })
 
-  return connect(mapStateToProps)(WithAuthInfo);
+  return connect(mapStateToProps)(Component)
 }
