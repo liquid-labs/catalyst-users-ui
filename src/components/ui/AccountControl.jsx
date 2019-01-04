8b625a9c0dd8111158b9024d780acd17f6365bc9
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-04 15:26:06 -0600

    AccountControls now uses dropdown for logout and profile
    as consequence, created 'withLogout' and changned 'Logout' to 'LogoutButton'

--- src/components/ui/AccountControl.jsx
+++ src/components/ui/AccountControl.jsx
@@ -7,32 +7,67 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox'
 import { AuthenticationDialog } from './AuthenticationDialog'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
-import { Logout } from './Logout'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { withAuthInfo } from '../hocs/withAuthInfo'
+import { withLogout } from '../hocs/withLogout'
+import { withRouter } from 'react-router-dom'
 
-const AccountControlBase = ({authUser, authenticationDialogOpen, setAuthenticationDialogOpen}) =>
-  authUser
-    ? <div key="profileControls">
-      <IconButton>
-        <AccountBoxIcon />
-      </IconButton>
-      <Logout />
-    </div>
-    : <div key="authenticationControl">
-      <Button onClick={() => setAuthenticationDialogOpen(true)}>Login</Button>
-      <AuthenticationDialog open={authenticationDialogOpen}
-          onClose={() => setAuthenticationDialogOpen(false)} />
-    </div>
+const AccountControlBase = ({
+  authUser,
+  menuAnchor, setMenuAnchor,
+  authenticationDialogOpen, setAuthenticationDialogOpen,
+  logout, history }) => {
+  if (authUser) {
+    const closeMenu = () => setMenuAnchor(null)
+    const menuLogout = () => {
+      closeMenu()
+      logout()
+    }
+    const menuProfile = () => {
+      closeMenu()
+      history.push('/users/self/profile')
+    }
+    return (
+      <div key="profileControls">
+        <IconButton onClick={(e) => setMenuAnchor(e.target)}>
+          <AccountBoxIcon />
+        </IconButton>
+        <Menu
+            anchorEl={menuAnchor}
+            open={Boolean(menuAnchor)}
+            onClose={closeMenu}
+        >
+          <MenuItem onClick={menuLogout}>Logout</MenuItem>
+          <MenuItem onClick={menuProfile}>Profile</MenuItem>
+        </Menu>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div key="authenticationControl">
+        <Button onClick={() => setAuthenticationDialogOpen(true)}>Login</Button>
+        <AuthenticationDialog open={authenticationDialogOpen}
+            onClose={() => setAuthenticationDialogOpen(false)} />
+      </div>
+    )
+  }
+}
 
 AccountControlBase.propTypes = {
   authUser                    : PropTypes.object,
   authenticationDialogOpen    : PropTypes.bool.isRequired,
-  setAuthenticationDialogOpen : PropTypes.func.isRequired
+  setAuthenticationDialogOpen : PropTypes.func.isRequired,
+  logout                      : PropTypes.func.isRequired,
 }
 
 const AccountControl = compose(
   withAuthInfo,
+  withRouter,
+  withLogout,
+  withState('menuAnchor', 'setMenuAnchor', null),
   withState('authenticationDialogOpen', 'setAuthenticationDialogOpen', false)
 )(AccountControlBase)
 
--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ export * from './components/ui/AuthenticationDialog'
 export * from './components/ui/PasswordChange'
 export * from './components/ui/PasswordRecoverForm'
 export * from './components/ui/LoginForm'
-export * from './components/ui/Logout'
+export * from './components/ui/LogoutButton'
 export * from './components/ui/Register'
 export * from './components/ui/UserAccount'
 
 export * from './components/hocs/checkAccess'
 export * from './components/hocs/manageAuthentication'
 export * from './components/hocs/withAuthInfo'
+export * from './components/hocs/withLogout'
 
 export * from './reducers/sessionReducer'
 export { sessionActions }
