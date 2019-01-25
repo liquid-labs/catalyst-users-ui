f8acbc8c4b5839786e192807bf8735ec4af20445
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-25 09:40:03 -0600

    wait on initial authentication to settle
    without waiting, we would get 'unauthorized warnings' when loading pages with 'accessCheck' based on the authUser

--- src/components/hocs/manageAuthentication.jsx
+++ src/components/hocs/manageAuthentication.jsx
@@ -1,9 +1,11 @@
-import { compose, lifecycle } from 'recompose'
+import { compose, lifecycle, withState } from 'recompose'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { appActions } from '@liquid-labs/catalyst-core-ui'
 import * as sessionActions from '../../actions/sessionActions'
+
+import { withAwait } from '@liquid-labs/catalyst-core-ui'
 
 import { fireauth } from '@liquid-labs/catalyst-firewrap'
 
@@ -16,29 +18,38 @@ const mapDispatchToProps = (dispatch) => ({
 const manageAuthentication = compose(
   withRouter,
   connect(null, mapDispatchToProps),
+  withState('authStatusChecked', 'setAuthStatusChecked', false),
   lifecycle({
     componentDidMount() {
-      const { logIn, logOut, setErrorMessage, history, location, postLoginStatusChangeUrl, postLoginUrl, postLogoutUrl } = this.props
+      const { logIn, logOut,
+        setAuthStatusChecked, setErrorMessage,
+        history, location, postLoginStatusChangeUrl, postLoginUrl, postLogoutUrl } = this.props
 
       fireauth.onAuthStateChanged((authUser) => {
         if (authUser) {
           authUser.getIdTokenResult().then(function(tokenInfo) {
             logIn(authUser, tokenInfo)
-          }).catch(function(error) {
-            console.warn(error) // eslint-disable-line no-console
+          })
+          .catch(function(error) {
+            console.warn('Error getting authentication token', error) // eslint-disable-line no-console
             setErrorMessage("Could not get token info; login invalidated.")
             logOut()
 
             history.push(postLoginUrl || postLoginStatusChangeUrl || '/')
           })
+          .finally(
+            () => setAuthStatusChecked(true)
+          )
         }
         else {
           logOut()
+          setAuthStatusChecked(true)
           if (location.pathname !== postLogoutUrl) {history.push(postLogoutUrl || postLoginStatusChangeUrl || '/')}
         }
       })
     }
-  })
+  }),
+  withAwait(({authStatusChecked}) => !authStatusChecked)
 )
 
 export { manageAuthentication }
