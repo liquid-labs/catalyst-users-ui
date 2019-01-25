266d365e08a880cc0b21d64e8fea24bb92650a5c
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-25 15:02:54 -0600

    sketched out Profile with CardContainer and card sections

--- src/components/ui/UserProfile.jsx
+++ src/components/ui/UserProfile.jsx
@@ -3,21 +3,41 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { compose } from 'recompose'
 
-import { AccountControl } from '@liquid-labs/catalyst-users-ui'
+import { AccountControl } from './AccountControl'
 import { BasicContentFrame } from '@liquid-labs/catalyst-core-ui'
+import { CardContainer, SectionGrid } from '@liquid-labs/mui-extensions'
+import { ContentHeader } from '@liquid-labs/catalyst-theme'
 import { PasswordRecoverForm } from './PasswordRecoverForm'
 import { PasswordChange } from './PasswordChange'
+import { ValidInput } from '@liquid-labs/react-validation'
 
 import { checkAccess } from '../hocs/checkAccess'
 import { withAuthInfo } from '../hocs/withAuthInfo'
 
 const UserProfileBase = ({ authUser, match }) =>
   <BasicContentFrame AppNavigationProps={{ logoTo : '/', rightChildren : <AccountControl /> }}>
-    <h1>Account Profile: {authUser.email}</h1>
-    { (match.params.id == 'self' || match.params.id == authUser.uid) &&
-      <PasswordChange /> }
-    { (match.params.id != 'self' && match.params.id != authUser.uid) &&
-      <PasswordReset /> }
+    <ContentHeader>{authUser.email}</ContentHeader>
+    <CardContainer>
+      <SectionGrid title="General">
+        <ValidInput
+          label="Display name"
+          value={authUser.displayName}
+          maxLength="255"
+          gridded={{xs: 12}}
+          viewOnly
+          defaultViewValue="<none>"
+        />
+      </SectionGrid>
+      <SectionGrid title="Authentication">
+        <ValidInput
+          label="Email verified"
+          value={authUser.emailVerified ? "yes" : "no"}
+          maxLength="3"
+          gridded={{xs: 12}}
+          viewOnly
+        />
+      </SectionGrid>
+    </CardContainer>
   </BasicContentFrame>
 
 const mapStateToProps = (state) => ({
