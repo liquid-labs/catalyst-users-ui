3c1000b605a2568b08448101af80cb53dfbd44fb
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-16 17:08:49 -0500

    removed divider and orgranized imports

--- js/components/widgets/AccountControlWidget.js
+++ js/components/widgets/AccountControlWidget.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 
 import AccountBoxIcon from '@material-ui/icons/AccountBox'
-import { useAuthenticationStatus, useAuthenticationAPI } from '@liquid-labs/catalyst-core-ui'
 import { AuthenticationDialog } from './AuthenticationDialog'
 import Button from '@material-ui/core/Button'
-import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
+
+import { useAuthenticationStatus, useAuthenticationAPI } from '@liquid-labs/catalyst-core-ui'
 
 import { withRouter } from 'react-router-dom'
 
